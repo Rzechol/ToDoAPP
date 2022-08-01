ed544ddc9727d95763eeac63e110a095e81d4071
Bartosz Rzechółka <27322@s.am.szczecin.pl>
2022-08-01 17:39:37 +0200

    Initial Commit

---
+++ ToDoAPP/ToDoAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAPPApp.swift
+//  ToDoAPP
+//
+//  Created by Bartosz Rzechółka on 01/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
